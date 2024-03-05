f52ccf2c7b225b952ba5d3ebc38e1e7c1a787b01
Meowu <474384902@qq.com>
2024-03-05 13:41:36 +0800

    chore.

--- scheme/Chapter 01/e1.1.scm
+++ scheme/Chapter 01/e1.1.scm
@@ -1,9 +1,8 @@
-# yici1shuru1
 10
 
 +( 5 3 4)
 
-(- 9 1) 
+(- 9 1)
 
 (/6 2)
 
@@ -13,20 +12,20 @@
 
 (define b (+ a 1))
 
-(+ a b (* a b)) 
+(+ a b (* a b))
 (= a b)
 
 (if (and (>b a ) (< b (* a b ) ) )
-    b 
+    b
     a)
 
 (cond ((= a 4 ) 6)
-      ((= b 4) (+ 6 7 a)) 
+      ((= b 4) (+ 6 7 a))
       (else 25))
 
 (+ 2 (if (> b a) b a))
 
-(* (cond ((> a b) a) 
+(* (cond ((> a b) a)
          ((< a b) b)
          (else -1))
    (+ a 1))
